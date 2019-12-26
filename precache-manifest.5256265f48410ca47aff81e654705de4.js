d556f233235d18ff58f3a53b6b27f65cc6c67058
Jekos <jhonny.lbk@gmail.com>
2019-12-26 19:22:31 +0200

    Updates

---
+++ precache-manifest.5256265f48410ca47aff81e654705de4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e82e704cc12f6140c472d1069434732d",
+    "url": "/fe_final/index.html"
+  },
+  {
+    "revision": "d986cec97a9c92fd467f",
+    "url": "/fe_final/static/css/main.a042a434.chunk.css"
+  },
+  {
+    "revision": "88aae9e541f82e34d3dd",
+    "url": "/fe_final/static/js/2.c6f003cf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/fe_final/static/js/2.c6f003cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d986cec97a9c92fd467f",
+    "url": "/fe_final/static/js/main.6dfc2104.chunk.js"
+  },
+  {
+    "revision": "5aa72635fa3400531485",
+    "url": "/fe_final/static/js/runtime-main.007d4d31.js"
+  },
+  {
+    "revision": "1cd294a771f26752bbb8d8d5210f6412",
+    "url": "/fe_final/static/media/Nunito-Bold.1cd294a7.ttf"
+  },
+  {
+    "revision": "4f3d2e011a08fae118e258bcaf0dfa13",
+    "url": "/fe_final/static/media/Nunito-ExtraLight.4f3d2e01.ttf"
+  },
+  {
+    "revision": "d2e691bc4a2b696929172cb3d22ce8ba",
+    "url": "/fe_final/static/media/Nunito-Regular.d2e691bc.ttf"
+  }
+]);
